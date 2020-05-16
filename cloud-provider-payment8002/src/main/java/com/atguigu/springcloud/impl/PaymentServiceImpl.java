2fc9334f11bd4adc2d7bb2c3b976ae0c3c9c455a
niejun <1289680419@qq.com>
2020-05-16 19:25:25 +0800

    去除事务测试内容

--- cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/impl/PaymentServiceImpl.java
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/impl/PaymentServiceImpl.java
@@ -4,7 +4,6 @@ import com.atguigu.springcloud.dao.PaymentDao;
 import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
